1f2a88f040a534b2374eb613cad199a2c0c567ef
Pedro Martins <t-pedro@iabs.org.br>
2019-01-30 11:37:41 -0200

    Banco de dados de "produção"

--- redeilpf/settings/stage.py
+++ redeilpf/settings/stage.py
@@ -1,24 +1,23 @@
 from .base import *
 
-#DATABASES
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'pe_chapeudepalha',
-#         'HOST': '192.168.3.206',
+#         'NAME': 'redeilpf_desenv',
+#         'HOST': '127.0.0.1',
 #         'PORT': '5432',
-#         'USER': 'pe_chapeudepalha',
-#         'PASSWORD': 'pe_chapeudepalha'
+#         'USER': 'redeilpf',
+#         'PASSWORD': 'redeilpf'
 #     }
 # }
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'redeilpf_desenv',
-        'HOST': '127.0.0.1',
+        'NAME': 'dlqbi77r2l1vu',
+        'HOST': 'ec2-107-20-185-27.compute-1.amazonaws.com',
         'PORT': '5432',
-        'USER': 'redeilpf',
-        'PASSWORD': 'redeilpf'
+        'USER': 'ebtdsndjycvrvw',
+        'PASSWORD': '74e218abb2c836295647778f1076b375f31114ff574bc0b37580031dd0f77b12'
     }
 }
